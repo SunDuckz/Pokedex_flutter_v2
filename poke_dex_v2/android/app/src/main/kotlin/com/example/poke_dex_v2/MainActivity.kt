2812489398db8709c6cac9c56add4d2d5b310bd8
Guilherme Bampi <124407613+SunDuckz@users.noreply.github.com>
2025-03-05 15:21:55 -0300

    Delete poke_dex_v2 directory

--- poke_dex_v2/android/app/src/main/kotlin/com/example/poke_dex_v2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.poke_dex_v2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- poke_dex_v2/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:pokedex/poke_dax_app.dart';
-
-void main() {
-  runApp(const PokeDexApp());
-}
